c2a4222585c6eb8e50f849ac0184a70232aedc24
hshima101 <67283100+hshima101@users.noreply.github.com>
2024-03-04 19:59:42 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
-const imagePaths = ['images/testImages/hog.png', 'images/testImages/rofl.jpg', 'images/testImages/horse.jpg']
-
-let currentIndex = 0;
-
-function changeBackground(){
-    const backgroundSection = document.getElementById('backgroundSection');
-    backgroundSection.style.backgroundImage = `url('${imagePaths[currentIndex]}')`;
-    currentIndex = (currentIndex + 1) % imagePaths.length;
+function addtocart(name){
+var cart=getcart()
+cart.push(name)
+setcart(cart)
 }
 
-setInterval(changeBackground, 5000);
+function getcart(){
+var cartdata=localStorage.getItem("cart")
+if (!cartdata){
+    return []
+}
+return JSON.parse(cartdata)
+}
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    var underConstructionElement = document.getElementById('underconstructioon');
-    underConstructionElement.style.display = 'block';
-});
+function setcart(cart){
+var cartdata=JSON.stringify(cart)
+localStorage.setItem("cart" , cartdata)
+updatepage(cart)
+}
+
+function updatepage(cart) {
+    var cartEl = document.querySelector('#cart');
+    cartEl.innerHTML = '';
+
+    for (let i = 0; i < cart.length; i++) {
+        var itemEl = document.createElement('div');
+        itemEl.innerText = cart[i];
+        itemEl.addEventListener('click', function() { removefromcart(i); });
+
+        cartEl.appendChild(itemEl);
+    }
+}
+
+function removefromcart(index) {
+    var cart = getcart();
+    cart.splice(index, 1);
+    setcart(cart);
+}
+
+updatepage(getcart())
+
+var page = window.location.pathname
+if (page == "/"){
+    page = "/index.html"
+}
+document.querySelector('a[href="' + page + '"]').setAttribute('class', 'current-page');
+if (page == "/index.html"){
+let images = [
+    "bun5.png",
+    "bun6.png",
+    "bun7.png"
+  ];
+  let currentIndex = 0;
+  function showImage() {
+    let imageElement = document.getElementById("image-carousel");
+    imageElement.src = "/images/" + images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length;
+  }
+  setInterval(showImage, 3000); // change image every 3 seconds
+  showImage()
+}
